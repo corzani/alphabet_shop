567f9b903f1fefb81c53563e6a3219798cbfd04f
corzani <corzani@gmail.com>
2022-12-18 00:11:56 +0000

    Updates documentation

--- src/main/kotlin/com/alphabet/shop/Offers.kt
+++ src/main/kotlin/com/alphabet/shop/Offers.kt
@@ -1,5 +1,6 @@
 package com.alphabet.shop
 
+// Considering our specs, this kind of offer is not needed. It was the very first case for tests so I left it.
 fun buyOneGetOneFree(quantity: Int, price: Price): Price = ((quantity / 2) + (quantity % 2)) * price
 
 fun groupDiscountOffer(take: Int, pay: Int) =
@@ -12,8 +13,8 @@ fun multiPricedOffer(groupQuantity: Int, groupCost: Price) =
 fun noOffer(quantity: Int, price: Price): Price = price * quantity
 
 /*
-Those function should filter the checkout item quantity in order to let other offers work on
-the remaining products
+Those functions should filter the checkout item quantity in order to let other offers work on
+the remaining products.
 If we have a meal deal on A and B, and we buy 2 A and 1 B, I might want to apply a possible offer over the
 remaining A that is not an aggregate offer
  */
